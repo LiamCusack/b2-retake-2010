4081d211f3fd7eef26da9de4bca7d011a1c1743c
Liam Cusack <LCusack8108@gmail.com>
2021-01-20 11:50:33 -0700

    Add method for sorting flight by most passengers. Added seeds and seeded DB. updated view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :itineraries, dependent: :destroy
   has_many :passengers, through: :itineraries
+
+  def self.order_by_most_passengers
+    select('flights.*, count(passengers.id) as passenger_count').joins(:passengers).group('flights.id').order('passenger_count desc')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Itinerary.destroy_all
+Passenger.destroy_all
+Flight.destroy_all
+Airline.destroy_all
+
+@a1 = Airline.create!(name: "Frontier")
+@f1 = Flight.create!(number: 1, date: "08/03/20", time: "08:00", departure_city: "Denver", arrival_city: "Aurora", airline_id: @a1.id)
+@f2 = Flight.create!(number: 2, date: "08/03/20", time: "09:00", departure_city: "Milwakee", arrival_city: "Denver", airline_id: @a1.id)
+@p1 = Passenger.create!(name: "Abracadaniel", age: 900)
+@p2 = Passenger.create!(name: "Jonnifer", age: 68)
+@p3 = Passenger.create!(name: "Bercy Hamhands", age: 7)
+@i1 = Itinerary.create!(flight_id: @f1.id, passenger_id: @p1.id)
+@i2 = Itinerary.create!(flight_id: @f1.id, passenger_id: @p2.id)
+@i3 = Itinerary.create!(flight_id: @f2.id, passenger_id: @p3.id)
+@i4 = Itinerary.create!(flight_id: @f1.id, passenger_id: @p3.id)
